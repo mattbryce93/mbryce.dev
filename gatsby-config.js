0f81c9630684a15618eda71f1227bc3be456e2f4
Matthew Bryce <mattbryce93@gmail.com>
2020-04-27 13:52:13 +1200

    Changed layout for github pages

--- gatsby-config.js
+++ gatsby-config.js
@@ -2,7 +2,7 @@ module.exports = {
   siteMetadata: {
     title: 'Gatsby Starter - Dimension V2',
     author: 'Hunter Chang',
-    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP',
+    description: 'A Gatsby.js V2 Starter based on Dimension by HTML5 UP'
   },
   plugins: [
     'gatsby-plugin-react-helmet',
@@ -15,9 +15,9 @@ module.exports = {
         background_color: '#663399',
         theme_color: '#663399',
         display: 'minimal-ui',
-        icon: 'src/images/mbryce-icon.png', // This path is relative to the root of the site.
-      },
+        icon: 'src/images/mbryce-icon.png' // This path is relative to the root of the site.
+      }
     },
-    'gatsby-plugin-sass',
-  ],
+    'gatsby-plugin-sass'
+  ]
 }
